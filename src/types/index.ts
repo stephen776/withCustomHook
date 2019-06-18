9764ab3ef3657ee37c7311e0fe414238bab7bb38
Stephen Jacobs <sjacobs@truefit.io>
2019-06-18 18:01:01 -0400

    adjusting api to contrain the hook params to be a single object type to get type inference working on the output HOC

--- src/types/index.ts
+++
@@ -1,4 +0,0 @@
-// signature for custom hook that accepts arbitrary params and returns some object
-// export type CustomHook<TParams extends (...args: any[]) => any, TResult> = (
-//   ...args: Parameters<TParams>
-// ) => TResult;
